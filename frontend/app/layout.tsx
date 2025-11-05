ac3c1796ca6511f9f684e2309c3f34734395045c
wgusta <gueney.usta@stud.hslu.ch>
2025-11-05 17:09:22 +0100

    sketchy design re-design

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -14,6 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;600;700&family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+      </head>
       <body>
         {children}
         <MatomoScript />
