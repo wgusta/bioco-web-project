63ed43be060c9fe52f20f6370817ac57b74f8066
wgusta <gueney.usta@stud.hslu.ch>
2025-11-10 17:29:21 +0100

    Update SVG paths to use full width (0-100) for all bento box types
    - Apple, carrot, and beet SVG paths now use full width (0-100) instead of 0-30
    - This ensures bento boxes are ALWAYS wider than their content
    - Organic curves maintained while maximizing inner space
    - Content should no longer be clipped by SVG shape boundaries

--- frontend/components/OrganicBentoBox.tsx
+++ frontend/components/OrganicBentoBox.tsx
@@ -16,14 +16,14 @@ export function OrganicBentoBox({ type, colSpan = 1, rowSpan = 1, className = ''
   const getSVGPath = () => {
     switch (type) {
       case 'apple':
-        // Horizontal apple shape - full width (0-100%)
-        return "M 0 50 Q 0 20 0 5 Q 0 0 0 0 Q 0 0 5 0 Q 10 0 15 0 Q 20 0 25 0 Q 30 0 30 5 Q 30 20 30 50 Q 30 80 30 95 Q 30 100 25 100 Q 20 100 15 100 Q 10 100 5 100 Q 0 100 0 95 Q 0 80 0 50 Z"
+        // Horizontal apple shape - FULL width (0-100) with organic curves
+        return "M 0 50 Q 0 20 0 5 Q 0 0 5 0 Q 10 0 15 0 Q 20 0 25 0 Q 30 0 35 0 Q 40 0 45 0 Q 50 0 55 0 Q 60 0 65 0 Q 70 0 75 0 Q 80 0 85 0 Q 90 0 95 0 Q 100 0 100 5 Q 100 20 100 50 Q 100 80 100 95 Q 100 100 95 100 Q 90 100 85 100 Q 80 100 75 100 Q 70 100 65 100 Q 60 100 55 100 Q 50 100 45 100 Q 40 100 35 100 Q 30 100 25 100 Q 20 100 15 100 Q 10 100 5 100 Q 0 100 0 95 Q 0 80 0 50 Z"
       case 'carrot':
-        // Horizontal carrot shape - full width (0-100%)
-        return "M 0 50 Q 0 20 0 5 Q 0 0 0 0 Q 0 0 5 0 Q 10 0 15 0 Q 20 0 25 0 Q 30 0 30 5 Q 30 20 30 50 Q 30 80 30 95 Q 30 100 25 100 Q 20 100 15 100 Q 10 100 5 100 Q 0 100 0 95 Q 0 80 0 50 Z"
+        // Horizontal carrot shape - FULL width (0-100) with organic curves
+        return "M 0 50 Q 0 20 0 5 Q 0 0 5 0 Q 10 0 15 0 Q 20 0 25 0 Q 30 0 35 0 Q 40 0 45 0 Q 50 0 55 0 Q 60 0 65 0 Q 70 0 75 0 Q 80 0 85 0 Q 90 0 95 0 Q 100 0 100 5 Q 100 20 100 50 Q 100 80 100 95 Q 100 100 95 100 Q 90 100 85 100 Q 80 100 75 100 Q 70 100 65 100 Q 60 100 55 100 Q 50 100 45 100 Q 40 100 35 100 Q 30 100 25 100 Q 20 100 15 100 Q 10 100 5 100 Q 0 100 0 95 Q 0 80 0 50 Z"
       case 'beet':
-        // Horizontal beetroot shape - full width (0-100%)
-        return "M 0 50 Q 0 20 0 5 Q 0 0 0 0 Q 0 0 5 0 Q 10 0 15 0 Q 20 0 25 0 Q 30 0 30 5 Q 30 20 30 50 Q 30 80 30 95 Q 30 100 25 100 Q 20 100 15 100 Q 10 100 5 100 Q 0 100 0 95 Q 0 80 0 50 Z"
+        // Horizontal beetroot shape - FULL width (0-100) with organic curves
+        return "M 0 50 Q 0 20 0 5 Q 0 0 5 0 Q 10 0 15 0 Q 20 0 25 0 Q 30 0 35 0 Q 40 0 45 0 Q 50 0 55 0 Q 60 0 65 0 Q 70 0 75 0 Q 80 0 85 0 Q 90 0 95 0 Q 100 0 100 5 Q 100 20 100 50 Q 100 80 100 95 Q 100 100 95 100 Q 90 100 85 100 Q 80 100 75 100 Q 70 100 65 100 Q 60 100 55 100 Q 50 100 45 100 Q 40 100 35 100 Q 30 100 25 100 Q 20 100 15 100 Q 10 100 5 100 Q 0 100 0 95 Q 0 80 0 50 Z"
       case 'bushbean':
         // Long bush bean shape - elongated, organic
         return "M 2 50 Q 1 40 2 30 Q 3 20 5 15 Q 7 10 10 8 Q 13 6 16 6 Q 19 6 22 8 Q 25 10 27 15 Q 29 20 30 30 Q 31 40 30 50 Q 31 60 30 70 Q 29 80 27 85 Q 25 90 22 92 Q 19 94 16 94 Q 13 94 10 92 Q 7 90 5 85 Q 3 80 2 70 Q 1 60 2 50 Z"
